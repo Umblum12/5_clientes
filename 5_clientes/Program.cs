73525e4ef47d2c4e3dd466865a411b2390eb92a5
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:28:47 -0600

    Agregar archivos de proyecto.

---
+++ 5_clientes/Program.cs
@@ -0,0 +1,32 @@
+﻿Datos[] estudiante = new Datos[5];
+for (int i = 0; i <= 4; i++)
+{
+    Console.WriteLine("--------------------------------------------");
+    Console.Write("Nombre del Cliente: ");
+    estudiante[i].nombre = Console.ReadLine();
+    Console.Write("Apellido del Cliente: ");
+    estudiante[i].apellido = Console.ReadLine();
+    Console.Write("Sexo del Cliente: ");
+    estudiante[i].sexo = Console.ReadLine();
+    Console.Write("Edad del Cliente: ");
+    estudiante[i].edad = int.Parse(Console.ReadLine());
+    Console.WriteLine("--------------------------------------------");
+}
+
+for (int i = 0; i <= 4; i++)
+{
+    Console.WriteLine("*------------------------------------------*");
+    Console.WriteLine("Nombre: " + estudiante[i].nombre);
+    Console.WriteLine("Apellido: " + estudiante[i].apellido);
+    Console.WriteLine("Sexo: " + estudiante[i].sexo);
+    Console.WriteLine("Edad: " + estudiante[i].edad);
+    Console.WriteLine("*------------------------------------------*");
+}
+
+struct Datos
+{
+    public string nombre;
+    public string apellido;
+    public string sexo;
+    public int edad;
+}
